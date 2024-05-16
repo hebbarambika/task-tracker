2b1f9470d08bd111fb93979f7fc4d6f90b5e71f5
unknown <hebbarambika.k@gmail.com>
2024-05-16 19:52:04 +0530

    added .env to gitignore file

--- configs/db.js
+++ configs/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
 	try {
 		// mongodb+srv://username:password@cluster-name.mongodb.net/dbName
 		const MONGO_URI =
-			"mongodb+srv://rhebbar18:Ambika%40123@cluster0.wopr3j5.mongodb.net/bizcart";
+			"";
 		await mongoose.connect(MONGO_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
